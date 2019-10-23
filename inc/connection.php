5d1372ae38a70f578f288599ef82006648099e2c
Chizzy <chizoba.princess@gmail.com>
2019-10-23 11:34:23 -0400

    created a PDO connection to access database

---
+++ inc/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+    $db = new PDO('sqlite:'.__DIR__.'/blog.db');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo 'Unable to connect 😕 <br>';
+    echo $e->getMessage();
+    exit;
+}
